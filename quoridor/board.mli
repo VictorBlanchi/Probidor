04a27e07d066f0245f4a57be9f6e940949d556bb
MathisBD <mathis.bouverot@ens.psl.eu>
2024-03-24 16:45:47 +0100

    added the board data structure to quoridor

---
+++ quoridor/board.mli
@@ -0,0 +1,53 @@
+(* This module defines the central data structure of the engine.
+   The board tracks the walls placed so far, but does not deal with pawns. 
+   This is an imperative data structure : most operations modify it in place. *)
+
+
+(** The type of a position on the board, represented as a pair of coordinates [(row, col)]. 
+    Coordinates are in the range [0..rows-1] and [0..columns-1]. 
+    The rows start at the top and go down, and the columns start at the left and go right. *)
+type pos = int * int
+
+(** The type of a wall on the board.
+    The position [pos] is the upper-left cell among those directly adjacent to the wall. *)
+type wall =
+  { horizontal: bool
+  ; length: int 
+  ; pos: pos }
+
+(** The abstract type of boards. *)
+type t
+
+exception Wall_out_of_bounds
+exception Wall_overlap
+exception Wall_missing
+
+(* Get the size of a board. *)
+val rows : t -> int
+val columns : t -> int
+
+(** Create a fresh board. A board can have any number of rows and columns > 0. *)
+val make : rows:int -> columns:int -> t
+
+(** Get the edges corresponding to a wall, even if it is out of bounds. *)
+val wall_edges : wall -> (pos * pos) list
+
+(** Is a position in the bounds of the board ? *)
+val pos_in_board : t -> pos -> bool
+
+(** Is a wall in the bounds of the board ? *)
+val wall_in_board : t -> wall -> bool
+
+(** Add a vertical or horizontal wall to the board. 
+    Raises [Wall_out_of_bounds] if the wall placement is invalid.
+    Raises [Wall_overlap] if the wall would overlap with another. *)
+val add_wall : t -> wall -> unit
+
+(** Add a vertical or horizontal wall to the board. 
+    Raises [Wall_out_of_bounds] if the wall placement is invalid.
+    Raises [Wall_missing] if there is no wall here. *)
+val remove_wall : t -> wall -> unit
+
+(** Test if there a path between two positions, 
+    where the destination is specified as a predicate on positions. *)
+val reachable : t -> pos -> (pos -> bool) -> bool
