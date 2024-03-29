e2bbdef2497973abc98dfcdeb921a59b405847ef
Victor Blanchi <victor.blanchi@ens.psl.eu>
2024-03-29 16:44:24 +0100

    Add errors (in an ugly (?) way: next thing todo -> use monad)

--- quoridor/state.ml
+++ quoridor/state.ml
@@ -3,11 +3,11 @@
 (** Exception raised when a player attempts to place a wall without any remaining walls available. *)
 exception Out_of_walls
 
-(* (\** Exception raised when placing a wall obstructs a player's path to victory. *\) *)
-(* exception Game_is_blocked *)
+(** Exception raised when placing a wall obstructs a player's path to victory. *)
+exception Game_is_blocked
 
-(* (\** Exception raised when a player attempts an invalid or illegal move. *\) *)
-(* exception Illegal_move *)
+(** Exception raised when a player attempts an invalid or illegal move. *)
+exception Illegal_move of string
 
 (** A tag identifying the two players.
     Player A starts on top of the board, and player B start at the bottom of the board. *)
@@ -101,12 +101,14 @@ let pos_inactive (game : t) : Board.pos = (inactive_player game).pawn_pos
 let target_pos (game : t) (d : direction list) : Board.pos =
   List.fold_left Board.add_pos (pos_active game) (List.map pos_from_dir d)
 
-(** Check that the position the active player wants to reach is not occupied and exist in the game *)
+(** Check that the position the active player wants to reach is not occupied. Raised an error if the position does not exist in the board *)
 let is_free (game : t) (d : direction list) : bool =
   let p' = target_pos game d in
-  p' <> pos_active game
-  && p' <> pos_inactive game
-  && Board.pos_in_board game.board p'
+  if Board.pos_in_board game.board p'
+  then begin
+    p' <> pos_active game && p' <> pos_inactive game
+  end
+  else raise (Illegal_move "The target position does not exist in the board")
 
 (** Check that there is not wall preventing the active player to reach where he wants to go*)
 let can_pass (game : t) (dirs : direction list) : bool =
@@ -167,20 +169,20 @@ let decrement_walls (game : t) : unit =
 
 (** Check if moving the active player's pawn in the direction d is valid,
     and if yes return the new position of the pawn. *)
-let action_move_valid (game : t) (d : direction) : Board.pos option =
+let action_move_valid (game : t) (d : direction) : Board.pos =
   match d with
   | (N | E | S | W) as d ->
       if is_free game [ d ] && can_pass game [ d ]
-      then Some (target_pos game [ d ])
-      else if (* Maybe the other player is next to us and we can jump over him *)
+      then target_pos game [ d ]
+      else if (* Maybe he other player is next to us: maybe we can jump over him *)
               (not (is_free game [ d ]))
               && is_free game [ d; d ]
               && can_pass game [ d; d ]
-      then Some (target_pos game [ d; d ])
-      else None
+      then target_pos game [ d; d ]
+      else raise @@ Illegal_move "A wall obstructs the path"
   | (NE | SE | SW | NW) as d -> begin
       if not (is_free game [ d ])
-      then None
+      then raise @@ Illegal_move "Another player occupies that spot"
       else
         let d1, d2 = split_diag_dir d in
         let is_valid (d1 : direction) (d2 : direction) : bool =
@@ -191,8 +193,8 @@ let action_move_valid (game : t) (d : direction) : Board.pos option =
           (* A wall preventing us to jump directly over the inactive player *)
         in
         if is_valid d1 d2 || is_valid d2 d1
-        then Some (target_pos game [ d ])
-        else None
+        then target_pos game [ d ]
+        else raise @@ Illegal_move "TODO"
     end
 
 (** Check if the active player can place this wall. *)
@@ -206,27 +208,18 @@ let action_wall_valid (game : t) (w : Board.wall) : bool =
   with _ -> false
 
 (** Execute a given (valid) action. This modifies the state in place. *)
-let execute_action (game : t) (act : action) : bool =
+let execute_action (game : t) (act : action) : unit =
   match act with
-  | MovePawn d -> (
-      match action_move_valid game d with
-      | Some p ->
-          (active_player game).pawn_pos <- p;
-          true
-      | None -> false)
-  | PlaceWall w -> (
-      try
-        if remaining_walls game <= 0 then raise Out_of_walls;
-        Board.add_wall game.board w;
-        let game_is_blocked = is_blocked game in
-        if game_is_blocked
-        then (
-          Board.remove_wall game.board w;
-          false)
-        else (
-          decrement_walls game;
-          true)
-      with _ -> false)
+  | MovePawn d -> (active_player game).pawn_pos <- action_move_valid game d
+  | PlaceWall w ->
+      if remaining_walls game <= 0 then raise Out_of_walls;
+      Board.add_wall game.board w;
+      let game_is_blocked = is_blocked game in
+      if game_is_blocked
+      then (
+        Board.remove_wall game.board w;
+        raise Game_is_blocked)
+      else decrement_walls game
 
 (** Generate the list of all VALID actions. *)
 let valid_actions (game : t) : action list =
@@ -236,7 +229,10 @@ let valid_actions (game : t) : action list =
       (fun d -> MovePawn d)
       (List.filter
          (fun d ->
-           match action_move_valid game d with None -> false | Some _ -> true)
+           try
+             ignore (action_move_valid game d);
+             true
+           with _ -> false)
          directions)
   in
   let walls = Board.generate_walls game.board game.wall_length in
