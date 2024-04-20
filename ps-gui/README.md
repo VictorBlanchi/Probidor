bd4ccbb4843419adc86bdd83ddbb58615325934c
MathisBD <mathis.bouverot@ens.psl.eu>
2024-04-21 00:05:29 +0200

    changed the ps-gui to start from the purescript-halogen-template

--- ps-gui/README.md
+++ ps-gui/README.md
@@ -1,10 +1,27 @@
-# Purescript GUI
+# Halogen Template
 
-This folder defines a GUI for Probidor. The project uses Purescript, and more precisely the library Halogen.
-The GUI communicates to the game engine via sockets.
+### Initial Setup
 
-# Setting up the project
+To install the toolchain locally (reads `devDependencies` from `package.json`):
+```sh
+npm install
+```
 
-Assuming you have Node installed (otherwise install Node Version Manager) :
-$ npm install -g purescript
-$ npm install -g spago
+### Building
+
+You can now build the PureScript source code with:
+
+```sh
+# An alias for `spago build`
+npm run build
+```
+
+### Launching the App
+
+You can launch your app in the browser with:
+
+```sh
+# An alias for `parcel dev/index.html --out-dir dev-dist --open`
+npm run serve
+```
+
--- ps-gui/src/Main.purs
+++ ps-gui/src/Main.purs
@@ -1,10 +1,48 @@
-module Main where
+module Main (main) where
 
 import Prelude
-
 import Effect (Effect)
-import Effect.Console (log)
+import Halogen as H
+import Halogen.Aff (awaitBody)
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.VDom.Driver (runUI)
+import Halogen.HTML.Events as HE
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main =
+  HA.runHalogenAff do
+    body <- awaitBody
+    runUI component unit body
+
+type State
+  = Int
+
+data Action
+  = Increment
+  | Decrement
+
+component :: forall query input output m. H.Component query input output m
+component =
+  H.mkComponent
+    { initialState: initialState
+    , render: render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
+
+initialState :: forall input. input -> State
+initialState _ = 0
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render state =
+  HH.div_
+    [ HH.p_ [ HH.text "Hello world" ]
+    , HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
+    , HH.p_ [ HH.text $ show state ]
+    , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
+    ]
+
+handleAction :: forall output m. Action -> H.HalogenM State Action () output m Unit
+handleAction = case _ of
+  Increment -> H.modify_ \state -> state + 1
+  Decrement -> H.modify_ \state -> state - 1
